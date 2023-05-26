276d28bda69e1e6563990d4ecae8e1d0a4f3d440
rosh <roshbon@gmail.com>
2023-05-26 11:10:33 +0100

    api setting up

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+
+
+app.use(bodyParser.json())
+app.listen(process.env.PORT, () => {
+
+    console.log(`Server is running on port ${process.env.PORT}`)
+
+
+ })
