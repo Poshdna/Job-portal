a905e1172bc637b737443e59c6fb3f1d97cf065b
rosh <roshbon@gmail.com>
2023-05-29 16:10:08 +0100

    completd

---
+++ authorization.js
@@ -0,0 +1,9 @@
+const authorization = (apikey) => { 
+
+    if (!apikey || apikey !== process.env.API_KEY) { 
+        return false
+    }
+    return true
+}
+
+module.exports = authorization
--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const Joi = require('joi')
 const { v4: uuidv4 } = require('uuid')
 const sendGrid = require('@sendgrid/mail')
 sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const axios = require('axios')
+const authorization = require('./authorization')
 
 let customerStore = [
     // {
@@ -27,6 +30,19 @@ const otpStore = [
     //    date
     // }
 ]
+const jobApplicationStore = [
+    // {
+    //    fullname: "",
+    //    address: "",
+    //    email: ""
+    //    jobId: ""
+    //    yearsOfExperiece: "",
+    //    qualifications: "",
+    //    status: 
+ 
+    // }
+]
+ 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => { 
@@ -37,7 +53,7 @@ app.get('/', (req, res) => {
     })
 })
 
-app.post('/register', (req, res) => { 
+app.post('/register', async(req, res) => { 
  
     const { lastname, firstname, email, phone, password } = req.body
 
@@ -69,13 +85,30 @@ app.post('/register', (req, res) => {
         return 
     }
 
+    const responseSalt = await bcrypt.genSalt(saltRounds)
+    if (!responseSalt) {
+        res.status(500).json({
+            status: false,
+            message: 'Sorry , we cannot create account this time, try again later'
+        })
+        return
+    }
+    const responseHash  = await bcrypt.hash(password, responseSalt)
+    if (!responseHash) {
+        res.status(500).json({
+            status: false,
+            message: 'Sorry , we cannot create account this time, try again later'
+        })
+        return
+    }
     const customer = {
                 id: uuidv4(),
                 lastname,
                 firstname,
                 email,
                 phone,
-                password,
+                salt: responseSalt,
+                password: responseHash,
                 status: "in-active",
                 registeredDate: new Date()
     }
@@ -142,7 +175,7 @@ app.get('/verify/:email/:otp', (req, res) => {
     })
 
     customerStore = [...newCustomerStore]
-
+    sendEmail(email, 'Registration Successful', `Hi, We arev happy to have you onboard. Let do some awesome stuffs together`,)
     res.status(200).json({
         status: true,
         message: 'OTP verified successfully'
@@ -150,6 +183,7 @@ app.get('/verify/:email/:otp', (req, res) => {
     
 })
  
+
 app.get('/resend-otp/:email', (req, res) => { 
     const { email } = req.params
     if (!email) {
@@ -188,10 +222,77 @@ app.get('/resend-otp/:email', (req, res) => {
 })
 
 
-app.get('/customers', (req, res) => { 
-    
+app.post('/login', async(req, res) => {
+
+    const { emailOrPhone, password } = req.body
+
+    const LoginSchema = Joi.object({
+        emailOrPhone: Joi.string().required(),
+        password: Joi.string().required()
+    })
+
+    const { value, error } = LoginSchema.validate(req.body)
+    if (error != undefined) {
+        res.status(400).json({
+            status: false,
+            message: error.details[0].message
+        })
+
+        return
+    }
+
+    const customer = customerStore.find(data => data.email === emailOrPhone || data.phone === emailOrPhone)
+    if (!customer) {
+        res.status(400).json({
+            status: false,
+            message: 'Invalid email or password'
+        })
+        return
+    }
+   
+    const responseHash = await bcrypt.hash(password, customer.salt)
+    if (!responseHash) {
+        res.status(500).json({
+            status: false,
+            message: 'Sorry , you canaot login this time, try again later'
+        })
+        return
+    }
+
+    if (responseHash !== customer.password) {
+        res.status(400).json({
+            status: false,
+            message: 'Invalid email or password'
+        })
+        return
+    }
+
+    if (customer.status !== 'active') {
+        res.status(400).json({
+            status: false,
+            message: 'Account not verified, Kindly go verify your accoiunt'
+        })
+        return
+    }
+
+
+    res.status(200).json({
+        status: true,
+        message: 'Login successful'
+    })
+
+})
+ 
+
+app.get('/jobs', async (req, res) => {
+
     const { apikey } = req.headers
-    if (!apikey || apikey !== process.env.API_KEY) {
+    const length = req.query.length || 10
+    const category = req.query.category || ''
+    const company = req.query.company || ''
+
+    const response = authorization(apikey)
+    if (!response) {
         res.status(401).json({
             status: false,
             message: 'Unauthorized'
@@ -199,7 +300,113 @@ app.get('/customers', (req, res) => {
         return
     }
 
+    const result = await axios({
+        method: "get",
+        url: `${process.env.REMOTE_API_BASEURL}/remote-jobs?limit=${length}&category=${category}&company_name=${company}`
+    })
+ 
+    res.status(200).json({
+        status: true,
+        count: result.data.jobs.length,
+        data: result.data.jobs
+    })
 
+
+    // fetch('https://remotive.com/api/remote-jobs')
+    // .then(response => response.json())
+    // .then(data => {
+    //     res.status(200).json({
+    //         status: true,
+    //         data: data.jobs
+    //     })
+    // })
+
+
+
+ })
+
+
+app.get('/jobs/categories', async(req, res) => {
+
+    const result = await axios({
+        method: 'get',
+        url: `${process.env.REMOTE_API_BASEURL}/remote-jobs`
+    })
+  
+
+    // const response1 = await axios({
+    //     method: "get",
+    //     url: "https://remotive.com/api/remote-jobs",
+
+    // })
+
+   const jobCategories = result.data.jobs.map(item => item.category)
+
+    res.status(200).json({
+        status: true,
+        data: jobCategories
+    })
+
+
+
+})
+ 
+app.post('/job/apply', (req, res) => {
+
+     const applySchema = Joi.object({
+        fullname: Joi.string().required().min(4),
+        address: Joi.string().required().min(10),
+        email: Joi.string().email().required(),
+        jobId : Joi.string().required(),
+        yearsOfExperiece: Joi.number().required(),
+        qualifications : Joi.string().required().valid('SSCE', 'BSC', 'MSC')
+     })
+    
+    const { value, error } = applySchema.validate(req.body)
+
+    if (error !== undefined) {
+        res.status(400).json({
+            status: false,
+            message: error.details[0].message
+        })
+        return
+    }
+    
+    const { fullname, address, email, jobId, yearsOfExperiece, qualifications } = req.body
+    const job = {
+        id: uuidv4(),
+        fullname,
+        address,
+        email,
+        jobId,
+        yearsOfExperiece,
+        qualifications,
+        status: 'submitted',
+        date: new Date()
+    }
+
+    jobApplicationStore.push(job)
+
+    res.status(200).json({
+        status: true,
+        message: 'Job application submitted successfully'
+    })
+
+
+})
+
+
+app.get('/admin/customers', (req, res) => { 
+    
+    const { apikey } = req.headers
+    const response = authorization(apikey)
+    if (!response) {
+        res.status(401).json({
+            status: false,
+            message: 'Unauthorized'
+        })
+        return
+    }
 
         res.status(200).json({
             status: true,
